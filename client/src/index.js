9b25d0a158e3df2c6776a0a69afdf50ce695f38a
maison moa <maison.moa@gmail.com>
2018-05-19 23:16:19 -0400

    update client views

--- client/src/index.js
+++ client/src/index.js
@@ -4,19 +4,10 @@ import './views/css/styles.css';
 import {BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
-//Views
-import App from './views/App';
-import Home from './views/Home';
-import CurrentWeather from './views/CurrentWeather';
 
 ReactDOM.render(
 
-	<Router>
-	  <App>
-		  <Route exact path='/' component={Home}/>
-		  <Route exact path='/current-weather' component={CurrentWeather}/>
-	  </App>
-	</Router>
+
 
 ,document.getElementById('root'));
 
