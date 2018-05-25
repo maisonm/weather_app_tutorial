923bd4699594731502d4700231ab8b14669f0297
Maison Moa <30670082+maisonm@users.noreply.github.com>
2018-05-25 13:12:59 -0400

    Update CurrentWeather.js

--- client/src/views/CurrentWeather.js
+++ client/src/views/CurrentWeather.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+
+//Assets
 import ThunderStormIcon from './assets/weather_icons/01W.svg';
 import RainIcon from './assets/weather_icons/02W.svg';
 import SnowIcon from './assets/weather_icons/03W.svg';
 import ClearIcon from './assets/weather_icons/04W-DAY.svg';
 import CloudsIcon from './assets/weather_icons/05W.svg';
+import LoadingIcon from './assets/loading.svg';
 
 
 class CurrentWeather extends Component {
