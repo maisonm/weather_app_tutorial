faa95784067370df8b638fa8ddf93139508eade7
Maison Moa <30670082+maisonm@users.noreply.github.com>
2018-05-25 14:54:23 -0400

    Update server.js

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Import Routes directory
+require('./routes')(app);
+
+app.get('/', (req, res) => {
+	res.send('PORT 5000');
+})
+
+
+app.listen(port, (err) => {
+	if(err) { console.log(err) };
+	console.log('Listening on port ' + port);
+})
