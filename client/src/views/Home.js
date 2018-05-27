31d9ea08a7610a466f8f726c31f6c5eee9b274c3
Maison Moa <30670082+maisonm@users.noreply.github.com>
2018-05-26 22:26:16 -0400

    update home.js
    changed class component into functional component

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -1,33 +1,27 @@
 import React, { Component } from 'react';
 import LightningBolt from './assets/lightning.svg';
 
-class Home extends Component {
-	constructor(props) {
-		super(props);
-	}
+const Home = () => {
 
+   return (
+      <div>
+         <div className='header'>
+            <h2>Weather Forcast</h2>
+            <img src={LightningBolt}/>
+         </div>
 
-	render() {
-		return (
-		<div>
-			<div className='header'>
-				<h2>Weather Forcast</h2>
-				<img src={LightningBolt}/>
-			</div>
+         <div className="instructions">
+            <p>Enter a US zipcode below to get the current weather conditions for that area.</p>
+         </div>
 
-			<div className="instructions">
-				<p>Enter your zipcode below to get the current weather forcast in your area.</p>
-			</div>
-
-			<div className='zipcodeInput'>
-				<form method='POST' action='/search-location'>
-					<input type='text' placeholder='Enter zipcode..' name='zipcode'/>
-					<button>ENTER</button>
-				</form>
-			</div>
-		</div>
-		)
-	}
+         <div className='zipcodeInput'>
+	  <form method='POST' action='/search-location'>
+	     <input type='text' placeholder='Enter zipcode..' name='zipcode'/>
+	     <button>ENTER</button>
+	  </form>
+         </div>
+      </div>
+   )
 }
 
-export default Home;
+export default Home
