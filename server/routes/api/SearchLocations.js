448053eadce65ead47d9b62aa269d9cdd9fa471f
Maison Moa <30670082+maisonm@users.noreply.github.com>
2018-05-28 14:30:32 -0400

    Update SearchLocations.js

--- server/routes/api/SearchLocations.js
+++ server/routes/api/SearchLocations.js
@@ -18,7 +18,7 @@ module.exports = (app) => {
 	app.get('/search-location-weather', (req, res) => {
 		//build api URL with user zip
 		const baseUrl = 'http://api.openweathermap.org/data/2.5/weather?zip=';	
-		const apiId = '&appid=18408c043cb329f8ecfffd0558165a04&units=imperial';
+		const apiId = '&appid=<YOUR API KEY GOES HERE>&units=imperial';
 		const userLocation = (url1, url2, zipcode) => {
 
 		   let newUrl = url1 + zipcode + url2;
