857d9df554b64e41735395c2afa9ac64d3094a6f
Maison Moa <30670082+maisonm@users.noreply.github.com>
2018-05-28 11:14:14 -0400

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,21 @@ import ReactDOM from 'react-dom';
 import './views/css/styles.css';
 import {BrowserRouter as Router, Route } from 'react-router-dom';
 
+//Views
+import App from './views/App';
+import Home from './views/Home';
+import CurrentWeather from './views/CurrentWeather';
+import ErrorDisplay from './views/ErrorDisplay';
 
 ReactDOM.render(
 
-
+	<Router>
+	  <App>
+		  <Route exact path='/' component={Home}/>
+		  <Route exact path='/current-weather' component={CurrentWeather}/>
+		  <Route exact path='/error' component={ErrorDisplay}/>
+	  </App>
+	</Router>
 
 ,document.getElementById('root'));
 
