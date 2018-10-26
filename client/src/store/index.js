17f5b2a464cbaf99ecb915408d250c14444b5abf
koopdev <koop@koop.codes>
2018-10-26 13:18:27 -0700

    added redux-dev-tools and composeWithDevTools

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,16 +1,15 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import rootReducer from './reducers/index';
 import rootSaga from './sagas/index';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const sagaMiddleware = createSagaMiddleware();
 
-const reduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-
 const store = createStore(
   rootReducer,
-  compose(applyMiddleware(sagaMiddleware), reduxDevTools)
-);
+  composeWithDevTools(applyMiddleware(sagaMiddleware))
+  );
 
 sagaMiddleware.run(rootSaga);
 
